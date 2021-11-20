4d5b4d1d09db3339ac4f83eef7ad1d2d9057932a
Gayatribhole <34337323+Gayatribhole@users.noreply.github.com>
2021-11-20 16:07:19 +0530

    Add files via upload

---
+++ OOPS ASSIGNMENTS/ABSTRATION/BankOfMaharashtra.java
@@ -0,0 +1,10 @@
+package oppsAssignments;
+
+public class BankOfMaharashtra extends Banks
+
+{
+    public void displayDenominations()
+    {
+        System.out.println(" Bank Of Maharashtra avaiable denominations: Rs 100, 500, 1000");
+    }
+}
