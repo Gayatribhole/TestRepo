114e932212d01223b2efc4f585a967d720b021f7
Gayatri <gayatribhole75@gmail.com>
2021-11-21 01:11:40 +0530

    Version13

--- vendingmachine/VendingMachine_1.java
+++ vendingmachine/VendingMachine_1.java
@@ -1,101 +1,159 @@
 package vendingmachine;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class VendingMachine_1
 {
-    public static void main(String[] args)
+    Scanner sc = new Scanner(System.in);
+    int currentamt=3000;
+
+    public int Display()
     {
-        double currentamt = 3000;
-        while (true) {
-            int price;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("MENU : 1. PEPSI (RS. 35)   2. SPRITE (RS.25)  3. 7UP (RS. 30)");
-            System.out.println("Choose item : ");
-            int item_id = sc.nextInt();
 
-            switch (item_id) {
-                case 1:
-                    price = 35;
-                    System.out.println("PEPSI (RS. 35)");
-                    System.out.println("Enter amount : ");
-                    int amount = sc.nextInt();
-                    if (amount > price) {
-                        int balance = amount - price;
-                        System.out.println("Please collect balance amount of RS." + balance);
+        System.out.println("MENU : 1. PEPSI (RS. 35)   2. SPRITE (RS.25)  3. 7UP (RS. 30) 4. Amount Available"  );
+        System.out.println("Choose item : ");
+        int item_id = sc.nextInt();
+        return item_id;
+    }
 
-                        System.out.println("Despensing PEPSI");
-                        currentamt = currentamt + price;
+    public void ChooseProduct(int item_id)
+    {
+        int choice=item_id;
+        if(choice==1)
+            System.out.println("Your PEPSI is ready to dispatch . Please pay Rs. 35 : ");
 
-                        System.out.println("Updated Amount in vending machine is Rs. " + currentamt);
+        if(choice==2)
+            System.out.println("Your SPRITE is ready to dispatch . Please pay Rs. 25 : ");
 
-                    } else if (amount == price) {
-                        int balance = amount;
-                        System.out.println("Despensing PEPSI");
-                        currentamt = currentamt + price;
-                        System.out.println("Updated balance is Rs. " + currentamt);
-                    } else {
-                        System.out.println("Please enter right amount");
-                    }
+        if(choice==3)
+            System.out.println("Your 7UP is ready to dispatch . Please pay Rs. 30 : ");
 
-                    break;
+        if(choice==4)
+            System.out.println("You asked for total amount available in machine. Please wait : ");
 
-
-                case 2:
-                    price = 25;
-                    System.out.println("SPRITE (RS. 25)");
-                    System.out.println("Enter amount : ");
-
-                    amount = sc.nextInt();
-                    if (amount > price) {
-                        int balance = amount - price;
-                        System.out.println("Please collect balance amount of RS." + balance);
-
-                        System.out.println("Despensing SPRITE");
-                        currentamt = currentamt + price;
-
-                        System.out.println("Updated Amount in vending machine is Rs. " + currentamt);
-
-                    } else if (amount == price) {
-                        int balance = amount;
-                        System.out.println("Despensing SPRITE");
-                        currentamt = currentamt + price;
-                        System.out.println("Updated balance is Rs. " + currentamt);
-                    } else {
-                        System.out.println("Please enter right amount");
-                    }
-                    break;
-
-                case 3:
-                    price = 30;
-                    System.out.println("7UP (RS. 30)");
-                    System.out.println("Enter amount : ");
-                    amount = sc.nextInt();
-                    if (amount > price) {
-                        int balance = amount - price;
-                        System.out.println("Please collect balance amount of RS." + balance);
-
-                        System.out.println("Despensing 7UP");
-                        currentamt = currentamt + price;
-
-                        System.out.println("Updated Amount in vending machine is Rs. " + currentamt);
-
-                    } else if (amount == price) {
-                        int balance = amount;
-                        System.out.println("Despensing 7UP");
-                        currentamt = currentamt + price;
-                        System.out.println("Updated balance is Rs. " + currentamt);
-                    } else {
-                        System.out.println("Please enter right amount");
-                    }
-                    break;
-            }
-            System.out.println("Do you want to order again?   Yes     NO");
-            String con = sc.next();
-            if((con.equals("NO")) || (con.equals("no")))
-               System.exit(0);
-        }
-
+        if(choice > 4)
+            System.out.println("Please enter correct product");
 
     }
+
+    public void Payment(int item_id) {
+        int choice = item_id;
+        int price;
+        int payedAmount;
+        int returnableAmount;
+
+        if (choice == 1) {
+            price = 35;
+            System.out.println("Pay amount : ");
+            payedAmount = sc.nextInt();
+            if (payedAmount > price) {
+                returnableAmount = payedAmount - price;
+                System.out.println("Please collect balance amount of RS." + returnableAmount);
+
+                System.out.println("Despensing PEPSI");
+                currentamt = currentamt + price;
+
+                System.out.println("Updated Amount in vending machine is Rs. " + currentamt);
+
+            } else if (payedAmount == price) {
+                int balance = payedAmount;
+                System.out.println("Despensing PEPSI");
+                currentamt = currentamt + price;
+                System.out.println("Updated balance is Rs. " + currentamt);
+            } else
+                System.out.println("Please enter right amount");
+        }
+
+        if (choice == 2) {
+            price = 25;
+            System.out.println("Pay amount : ");
+            payedAmount = sc.nextInt();
+            if (payedAmount > price) {
+                returnableAmount = payedAmount - price;
+                System.out.println("Please collect balance amount of RS." + returnableAmount);
+
+                System.out.println("Despensing SPRITE");
+                currentamt = currentamt + price;
+
+                System.out.println("Updated Amount in vending machine is Rs. " + currentamt);
+
+            } else if (payedAmount == price) {
+                int balance = payedAmount;
+                System.out.println("Despensing PEPSI");
+                currentamt = currentamt + price;
+                System.out.println("Updated balance is Rs. " + currentamt);
+            } else
+                System.out.println("Please enter right amount");
+        }
+
+        if (choice == 3) {
+            price = 30;
+            System.out.println("Enter amount : ");
+            payedAmount = sc.nextInt();
+            if (payedAmount > price) {
+                returnableAmount = payedAmount - price;
+                System.out.println("Please collect balance amount of RS." + returnableAmount);
+
+                System.out.println("Despensing PEPSI");
+                currentamt = currentamt + price;
+
+                System.out.println("Updated Amount in vending machine is Rs. " + currentamt);
+
+            }
+            else if (payedAmount == price)
+            {
+                int balance = payedAmount;
+                System.out.println("Despensing PEPSI");
+                currentamt = currentamt + price;
+                System.out.println("Updated balance is Rs. " + currentamt);
+            }
+            else
+                System.out.println("Please enter right amount");
+            }
+
+        if (choice == 4)
+            System.out.println("Amount available is : " + currentamt);
+
+    }
+
+     String IsContinue()
+    {
+        System.out.println("Do you want to Buy another product ? ");
+        System.out.println(" Yes (y/Y)  No(n/N)");
+        String decision = sc.next();
+        if (decision.equals("y") || decision.equals("Y"))
+            return "y";
+        else
+            return "n";
+
+    }
+
+    public static void main(String[] args)
+    {
+        VendingMachine_1 vm= new VendingMachine_1();
+        /*while(1==1)
+        {
+            int item_id = vm.Display();
+            vm.ChooseProduct(item_id);
+            vm.Payment(item_id);
+            String decision = vm.IsContinue();
+            if(decision=="n")
+
+        }
+        */
+        while(true){
+            System.out.println("infinitive while loop");
+            int item_id = vm.Display();
+            vm.ChooseProduct(item_id);
+            vm.Payment(item_id);
+            String decision = vm.IsContinue();
+            if(decision.equals("n")||decision.equals("N"))
+            {
+                break;
+            }
+        }
+
+    }
+
 }
