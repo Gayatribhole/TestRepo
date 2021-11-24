b9b4f82881f4c9f68426ccba16c6e0b0570cc112
Gayatribhole <34337323+Gayatribhole@users.noreply.github.com>
2021-11-24 04:42:34 -0800

    Update StudyRoom.java

--- OOPS/Inheretence/StudyRoom.java
+++ OOPS/Inheretence/StudyRoom.java
@@ -5,9 +5,9 @@ public class StudyRoom extends Home
         public static void main(String[] args)
         {
             StudyRoom studyroom=new StudyRoom();
-           int a= studyroom.numberOfLights=3;
+           int numberOfLights=3;
            String roomName="StudyRoom";
-           studyroom.noOfLights( roomName,a);
+           studyroom.noOfLights( roomName,numberOfLights);
 
         }
 
