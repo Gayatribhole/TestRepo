3cbfbc558e96cd55c0f6a6595286df548e7cada9
Gayatribhole <34337323+Gayatribhole@users.noreply.github.com>
2021-11-24 04:42:03 -0800

    Update LivingRoom.java

--- OOPS/Inheretence/LivingRoom.java
+++ OOPS/Inheretence/LivingRoom.java
@@ -5,9 +5,9 @@ public class LivingRoom extends Home
     public static void main(String[] args)
     {
         LivingRoom livingroom = new LivingRoom();
-        int a= livingroom.numberOfLights=4;
+        int numberOfLights=4;
         String roomName ="LivingRoom";
-        livingroom.noOfLights(roomName,a);
+        livingroom.noOfLights(roomName,numberOfLights);
     }
 
 }
