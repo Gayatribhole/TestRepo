fe1b88e8bf1696d00c58aa7b584d0ab96a2f499e
Gayatribhole <34337323+Gayatribhole@users.noreply.github.com>
2021-11-24 05:02:09 -0800

    Update Banks.java

--- OOPS/Abstraction/Banks.java
+++ OOPS/Abstraction/Banks.java
@@ -4,10 +4,15 @@ public abstract class Banks
 {
     public abstract void displayDenominations()
     {
-        system.out.println("Denominations available in banks are displayed below: ");
+        system.out.println("Denominations available in bank are displayed below: ");
     }
     
+}
 
+
+
+public class AllBanks
+{
     public static void main(String[] args)
     {
         Banks canaraBank = new CanaraBank();
@@ -20,4 +25,3 @@ public abstract class Banks
         icici.displayDenominations();
     }
 }
-
