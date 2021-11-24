2df983e1a5f68568849e34be66db93ea4b7b3ff4
Gayatribhole <34337323+Gayatribhole@users.noreply.github.com>
2021-11-24 04:33:14 -0800

    Update CanaraBank.java

--- OOPS/Abstraction/CanaraBank.java
+++ OOPS/Abstraction/CanaraBank.java
@@ -2,16 +2,9 @@ package oppsAssignments;
 
 public class CanaraBank extends Banks
 {
-    @Override
     public void displayDenominations()
     {
         System.out.println("Canara Bank avaiable denominations: Rs 100, 500, 1000, 2000");
-
-    }
-
-    public static void main(String[] args)
-    {
-
 
     }
 
